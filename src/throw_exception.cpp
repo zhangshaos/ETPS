c42048c61883998a20511a768292faf2cbb96b98
zhangshaos <xingmingzhangssr@gmail.com>
2023-08-25 15:11:20 +0800

    a simple ETPS, the result is so bad.

---
+++ src/throw_exception.cpp
@@ -0,0 +1,5 @@
+//
+// Created by zxm on 2023/8/25.
+//
+
+#include "throw_exception.h"
---
+++ src/throw_exception.h
@@ -0,0 +1,16 @@
+//
+// Created by zxm on 2023/8/25.
+//
+
+#ifndef ETPS_SRC_THROW_EXCEPTION_H_
+#define ETPS_SRC_THROW_EXCEPTION_H_
+
+
+
+class throw_exception {
+
+};
+
+
+
+#endif //ETPS_SRC_THROW_EXCEPTION_H_
