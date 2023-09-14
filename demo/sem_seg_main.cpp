8d8e0ee501b7f1f27534308238881fb295d4aca5
zhangshaos <xingmingzhangssr@gmail.com>
2023-09-14 16:42:20 +0800

    ...

--- demo/sem_seg_main.cpp
+++ demo/sem_seg_main.cpp
@@ -12,44 +12,62 @@ img_dir   = "/mnt/f/XGrids/PHOTO/0_resized",
 label_dir = "/mnt/f/XGrids/PHOTO/0_result/pred",
 names[]   = {
     "085110107700026",
+    "085110107700059",
+    "085110107700068",
+    "085110107700083",
+    "085110107700146",
+    "085110107700200",
+    "085110107700240",
+    "085110107700298",
+    "085110107700345",
+    "085110107700392",
+    "085110107700551",
+    "085110107700651",
+    "085110107700695",
+    "085110107700737",
+    "085110107700807",
+    "085110107700859",
+    "085110107700865",
+    "085110107701010",
+    "085110107701050",
+    "085110107701057",
+    "085110107701119",
+    "085110107701155",
+    "085110107701208",
+    "085110107701217",
+    "085110107701225",
+    "085110107701227",
+    "085110107701272",
+    "085110107701282",
     "085110107701308",
 };
 
 
-
-int main(int argc, char *argv[]) {
-  printf("input the file index:\n");
-  fflush(stdout);
-  int i=0;
-  scanf("%d", &i);
-  if (i < 0 || i > std::size(names)){
-    printf("file index must to be in the range [%d, %zu).\n", 0, std::size(names));
-    return EXIT_SUCCESS;
-  }
-
+void
+main_(int i){
   auto img_path   = str_printf(512, "%s/%s.JPG", img_dir.c_str(), names[i].c_str()),
-       label_path = str_printf(512, "%s/%s.npy", label_dir.c_str(), names[i].c_str());
+      label_path = str_printf(512, "%s/%s.npy", label_dir.c_str(), names[i].c_str());
   cv::Mat_<cv::Vec3b>
-  rgb_img     = cv::imread(img_path);
+      rgb_img     = cv::imread(img_path);
   cv::cvtColor(rgb_img, rgb_img, cv::COLOR_BGR2RGB);
   cv::Mat_<float>
-  class_prob  = cvDNN::blobFromNPY(label_path, CV_32F);
+      class_prob  = cvDNN::blobFromNPY(label_path, CV_32F);
 
   HyperParams params1;
   params1.verbose = false;
   cv::Mat_<int>
-  spx_label   = ccs(rgb_img, params1);
+      spx_label   = ccs(rgb_img, params1);
 
   cv::Mat_<int>
-  class_label = naive_semantic_seg(spx_label, rgb_img, class_prob);
+      class_label = naive_semantic_seg(spx_label, rgb_img, class_prob);
 
   auto result_path1 = str_printf(512, "%s_naive_seg.png", names[i].c_str()),
-       result_path2 = str_printf(512, "%s_naive_color.png", names[i].c_str());
+      result_path2 = str_printf(512, "%s_naive_color.png", names[i].c_str());
   save_segmentation_map(result_path1, class_label);
   save_image_with_segmentation(result_path2, rgb_img, class_label);
 
   CRF_Params params2;
-  params2.wi = 10;
+  params2.verbose = false;
   class_label = crf_semantic_seg(spx_label, rgb_img, class_prob, params2);
 
   result_path1 = str_printf(512, "%s_crf_seg.png", names[i].c_str());
@@ -58,22 +76,27 @@ int main(int argc, char *argv[]) {
   save_image_with_segmentation(result_path2, rgb_img, class_label);
 
   MRF_Params params3;
-  params3.wi = 10;
+  params3.verbose = false;
   class_label = mrf_semantic_seg(spx_label, rgb_img, class_prob, params3);
 
   result_path1 = str_printf(512, "%s_mrf_seg.png", names[i].c_str());
   result_path2 = str_printf(512, "%s_mrf_color.png", names[i].c_str());
   save_segmentation_map(result_path1, class_label);
   save_image_with_segmentation(result_path2, rgb_img, class_label);
+}
 
-//  HyperParams params;
-//  params.expect_spx_num = 1000;
-//  params.verbose = true;
-//  namespace time = std::chrono;
-//  auto t0 = time::steady_clock::now();
-//  ccs(rgb_img, params);
-//  auto t1 = time::steady_clock::now();
-//  float used_sec = time::duration_cast<time::milliseconds>(t1 - t0).count() * 1e-3f;
-//  printf("ccs algorithm cost %.3f second", used_sec);
+
+int main(int argc, char *argv[]) {
+  printf("input the file index(0~%zu):\n"
+         "the index out of range means take all files.\n",
+         std::size(names)-1);
+  fflush(stdout);
+  int i=0;
+  scanf("%d", &i);
+  if (i < 0 || i > std::size(names))
+    for (int j=std::size(names)-1; j>=0; --j)
+      main_(j);
+  else
+    main_(i);
   return 0;
 }
